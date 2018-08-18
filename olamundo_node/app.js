bf6d4a19fc871e269b57b6f12df35931bb93cc76
Fabio Santos <programadornatal@gmail.com>
2018-08-17 23:57:48 -0300

    Update app.js

--- olamundo_node/app.js
+++ olamundo_node/app.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function(socket){
