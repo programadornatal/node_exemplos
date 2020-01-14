a1230b68025f731b0ef411d9630d20507689f65f
santos.f <programadornatal@gmail.com>
2020-01-14 16:32:37 -0400

    Criação da api rest

--- node_reactive_native/index.js
+++ node_reactive_native/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello React');
+    res.json({projeto:'Hello React'});
 });
 
 app.listen('3000');
