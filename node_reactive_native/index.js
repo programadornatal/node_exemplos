7fd48032c33866022394abe69285e8091b65e5f5
santos.f <programadornatal@gmail.com>
2020-01-15 08:03:57 -0400

    Get test e erro 404

--- node_reactive_native/index.js
+++ node_reactive_native/index.js
@@ -6,4 +6,12 @@ app.get('/', (req, res) => {
     res.json({projeto:'Hello React'});
 });
 
+app.get('/teste', (req, res) => {
+    res.send('Recebeu o teste');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Essa página não existe! :)');
+});
+
 app.listen('3000');
